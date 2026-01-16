bd51f6db9b50fa05b126f480208805705afd1a21
guoshuai <guoshuai@cmict.chinamobile.com>
2026-01-16 11:35:50 +0800

    workflows

--- utils/youtube.py
+++ utils/youtube.py
@@ -127,16 +127,14 @@ def get_video_content(video_id):
             'sleep_interval_requests': 10,
             'extractor_args': {
                 'youtube': {
-                     'player_client': ['ios'],
-                    'player_skip': ['webpage', 'configs', 'js'],
-                    'include_live_dash': False,
+                    'player_client': ['tv']
                 }
             },
-            'http_headers': {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                'Accept-Language': 'en-us,en;q=0.5',
-            }
+            # 'http_headers': {
+            #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+            #     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            #     'Accept-Language': 'en-us,en;q=0.5',
+            # }
             }
         if config.PROXY_URL:
             ydl_opts['proxy'] = config.PROXY_URL
